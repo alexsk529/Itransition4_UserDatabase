7c3a9d8894664ea77b49f5126c1372a8c2fc79e9
Alex529 <alexsk529@mail.ru>
2022-11-25 16:45:26 +0500

    Client Added

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.scss';
+import {useRoutes} from './routes.js';
+import {useAuth} from "./hooks/auth.hook.js";
+import {AuthContext} from "./context/AuthContext.js";
+
+function App() {
+    const {login, logout, token, id, isReady} = useAuth();
+    const isLogin = !!token;
+    const routes = useRoutes(isLogin);
+    return (
+        <AuthContext.Provider value={{login, logout, token, id, isReady}}>
+            <div className="app">
+                {routes}
+            </div>
+        </AuthContext.Provider>
+
+    );
+}
+
+export default App;
---
+++ client/src/pages/AuthPage/AuthPage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+
+import MainPage from "../MainPage/MainPage";
+import { ButtonGroup, Button, Container } from '@mui/material';
+import Signin from '../../components/Signin.js'
+import Signup from '../../components/Signup.js'
+import './AuthPage.scss'
+
+const AuthPage = () => {
+    const [form, setForm] = React.useState({
+        email: '',
+        password: '',
+    })
+    const [errorMessage, setErrorMessage] = React.useState('')
+    const handlerChange = (e) => {
+        setForm({...form, [e.target.name]:e.target.value});
+    }
+    return (
+
+            <React.Fragment>
+                <Container maxWidth="320px" sx={{pt: 3}}>
+                    <ButtonGroup variant="contained" aria-label="outlined primary button group" padding={150}>
+                        <Button><Link to="/signin">Sign in</Link></Button>
+                        <Button><Link to="/signup">Sing up</Link></Button>
+                    </ButtonGroup>
+                </Container>
+
+                <Routes>
+                    <Route path="/signin" element={<Signin form={form} handlerChange={handlerChange} errorMessage={errorMessage} setErrorMessage={setErrorMessage} />}/>
+
+                    <Route path="/signup" element={<Signup form={form} handlerChange={handlerChange} errorMessage={errorMessage} setErrorMessage={setErrorMessage} />}/>
+                    <Route path="/main" element={<MainPage/>}/>
+                </Routes>
+            </React.Fragment>
+
+
+    )
+}
+
+export default AuthPage
